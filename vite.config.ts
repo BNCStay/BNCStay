ef1bdb452e7d6d42c8564c6014d203bdf06c873d
Chidi Benedict <c4benni@gmail.com>
2024-03-08 10:52:01 +0100

    update index page

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 3500,
   },
-  base: 'https://bncstay.github.io/BNCStay/',
+  base: 'https://bncstay.github.io/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
